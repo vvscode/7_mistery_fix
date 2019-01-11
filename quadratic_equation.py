d971d1bd280f65d556527f292f124465b5885b07
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-11 19:13:58 +0400

    Fix login for quadratic_euqation

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,13 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
+        return None, None
+
     root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+
     if discriminant == 0:
         return root1, None
     else:
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
